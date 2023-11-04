818ec03274c07ea506d8e54c17c3f998d637cea7
JD Solanki <jdsolanki0001@gmail.com>
2023-11-05 00:53:59 +0530

    feat: added esc key handler to exist from top most stacked layout widget

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import sys
+from typing import cast, override
 
+from PyQt6.QtCore import QEvent, QObject, Qt
+from PyQt6.QtGui import QKeyEvent
 from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedLayout, QWidget
 
 from app.launcher import Launcher
@@ -6,7 +10,6 @@ from app.views.list_view import AListView
 
 
 class MainWindow(QMainWindow):
-
     def __init__(self, launcher: Launcher):
         super(MainWindow, self).__init__()
         self.init_launcher(launcher)
@@ -30,12 +33,34 @@ class MainWindow(QMainWindow):
         stacked_layout_widget.setLayout(self.stacked_layout)
         self.setCentralWidget(stacked_layout_widget)
 
+        # Install the event filter
+        self.installEventFilter(self)
+
     def init_launcher(self, launcher: Launcher):
         self.launcher = launcher
         self.launcher.load_plugins()
 
+    # ℹ️ a0 is obj & a1 is event
+    @override
+    def eventFilter(self, a0: QObject | None, a1: QEvent | None) -> bool:
+        if a1 is None or a1.type() != QEvent.Type.KeyPress:
+            return super().eventFilter(a0, a1)
+
+        key_event = cast(QKeyEvent, a1)
+        key: int = key_event.key()
+        if key != Qt.Key.Key_Escape:
+            return super().eventFilter(a0, a1)
+
+        if self.stacked_layout.currentIndex() > 0:
+            self.stacked_layout.setCurrentIndex(self.stacked_layout.currentIndex() - 1)
+        else:
+            QApplication.quit()
+
+        return True
+
+
 if __name__ == "__main__":
-    app = QApplication([])
+    app = QApplication(sys.argv)
     w = MainWindow(Launcher())
     w.show()
-    app.exec()
+    sys.exit(app.exec())
--- stacking.py
+++ stacking.py
@@ -1,3 +1,8 @@
+import sys
+from typing import cast, override
+
+from PyQt6.QtCore import QEvent, QObject, Qt
+from PyQt6.QtGui import QKeyEvent
 from PyQt6.QtWidgets import (
     QApplication,
     QMainWindow,
@@ -9,7 +14,7 @@ from PyQt6.QtWidgets import (
 
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+    def __init__(self) -> None:
         super(MainWindow, self).__init__()
 
         # Create a QVBoxLayout
@@ -18,11 +23,11 @@ class MainWindow(QMainWindow):
 
         # Create a QPushButton and add it to the QVBoxLayout
         go_in_btn = QPushButton("Go In")
-        go_in_btn.clicked.connect(lambda _: self.stacked_layout.setCurrentIndex(1)) # type: ignore
+        go_in_btn.clicked.connect(lambda _: self.stacked_layout.setCurrentIndex(1))  # type: ignore
         layout_1.addWidget(go_in_btn)
 
         go_out_btn = QPushButton("Go Out")
-        go_out_btn.clicked.connect(lambda _: self.stacked_layout.setCurrentIndex(0)) # type: ignore
+        go_out_btn.clicked.connect(lambda _: self.stacked_layout.setCurrentIndex(0))  # type: ignore
         layout_2.addWidget(go_out_btn)
 
         # Create a QWidget and set its layout to the QVBoxLayout
@@ -46,8 +51,30 @@ class MainWindow(QMainWindow):
         stacked_layout_widget.setLayout(self.stacked_layout)
         self.setCentralWidget(stacked_layout_widget)
 
+        # Install the event filter
+        self.installEventFilter(self)
+
+    # ℹ️ a0 is obj & a1 is event
+    @override
+    def eventFilter(self, a0: QObject | None, a1: QEvent | None) -> bool:
+        if a1 is None or a1.type() != QEvent.Type.KeyPress:
+            return super().eventFilter(a0, a1)
+
+        key_event = cast(QKeyEvent, a1)
+        key: int = key_event.key()
+        if key != Qt.Key.Key_Escape:
+            return super().eventFilter(a0, a1)
+
+        if self.stacked_layout.currentIndex() > 0:
+            self.stacked_layout.setCurrentIndex(self.stacked_layout.currentIndex() - 1)
+        else:
+            QApplication.quit()
+
+        return True
+
+
 if __name__ == "__main__":
-    app = QApplication([])
+    app = QApplication(sys.argv)
     w = MainWindow()
     w.show()
-    app.exec()
+    sys.exit(app.exec())
